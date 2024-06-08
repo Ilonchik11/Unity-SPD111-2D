e44f44839a2bf1094314a098cf6cbbc4b354024c
Ilonchik11 <ilonka7011@gmail.com>
2024-06-08 17:43:23 +0300

    Initial commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private Rigidbody2D rigidBody; // Reference на компонент того же ГО, на якому скрипт
+    void Start()
+    {
+        Debug.Log("BirdScript Start");
+        // пошук компонента та одержання посилання на нього 
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(new Vector2(0, 500));
+        }
+    }
+}
